a4c720ac43eb1072000b36768e65a72f55240ce4
Steven <github@steven-h.net>
2022-06-20 01:13:19 -0400

    microimprovements

--- scripts/captions/CaptionStorage.lua
+++ scripts/captions/CaptionStorage.lua
@@ -1,6 +1,15 @@
+local Timer = require "system.utils.Timer"
+
 local module = {}
 
 local CaptionList = {}
-local CaptionTypes = {}
 local CaptionPositions = {}
 local UnusedPositions = {}
+local FutureCaptions = {}
+
+function module.add(data)
+  local now = Timer.getGlobalTime()
+
+  table.insert(CaptionList, data)
+  table.sort(CaptionList, function(left, right) return left.timeAppear < right.timeAppear end)
+end
--- scripts/captions/SoundEvent.lua
+++ scripts/captions/SoundEvent.lua
@@ -4,27 +4,50 @@ local Event        = require "necro.event.Event"
 local GFX          = require "system.gfx.GFX"
 local Render       = require "necro.render.Render"
 
-local CaptionBank = require "AccessibleCrypt.captions.CaptionBank"
+local CaptionBank    = require "AccessibleCrypt.captions.CaptionBank"
+local CaptionStorage = require "AccessibleCrypt.captions.CaptionStorage"
 
 local sounds = {}
 
 Event.soundPlay.add("soundGroup", { order = "play", sequence = -1 }, function(ev)
   if ev.ui then return end
 
-  local caption = CaptionBank[ev.group]
+  local captionGroup = ev.group
+  local caption = CaptionBank[captionGroup]
+
+  if type(caption) == "table" then
+    captionGroup = caption.group or captionGroup
+    caption = caption[1]
+  end
+
+  if type(caption) == "function" then
+    caption = caption()
+  end
+
   if type(caption) == "string" then
     local output = caption
     local pos = not not (ev.x and ev.y)
+    local direction = 0
+
     if pos then
       local x, y = Camera.transformPoint(Render.tileCenter(ev.x, ev.y))
 
       local xPct = x / GFX.getWidth()
 
-      if xPct < 1 / 3 then output = "<= " .. output
-      elseif xPct > 2 / 3 then output = output .. " =>"
+      if xPct < 1 / 3 then direction = -1
+      elseif xPct > 2 / 3 then direction = 1
       end
     end
 
-    print(output)
+    local captionData = {
+      text = caption,
+      soundType = captionGroup,
+      timeAppear = ev.time,
+      timeFadeStart = ev.time + 5,
+      timeFadeEnd = ev.time + 7,
+      direction = direction
+    }
+
+    CaptionStorage.add(captionData)
   end
 end)
