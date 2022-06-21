d371312984e7e3d1ef6ae1fc2c212f1ccaeaf4d5
Steven <github@steven-h.net>
2022-06-21 13:04:23 -0400

    Some work

--- scripts/captions/CaptionStorage.lua
+++ scripts/captions/CaptionStorage.lua
@@ -1,4 +1,5 @@
-local Timer = require "system.utils.Timer"
+local Timer     = require "system.utils.Timer"
+local Utilities = require "system.utils.Utilities"
 
 local module = {}
 
@@ -10,6 +11,33 @@ local FutureCaptions = {}
 function module.add(data)
   local now = Timer.getGlobalTime()
 
-  table.insert(CaptionList, data)
-  table.sort(CaptionList, function(left, right) return left.timeAppear < right.timeAppear end)
+  if now < data.timeAppear then
+    module.addFuture(data)
+  else
+    module.addLive(data)
+  end
+end
+
+function module.addFuture(data)
+  -- Add to the future captions list
+  table.insert(FutureCaptions, data)
+
+  -- Sort by decreasing imminence (so the next sound to play should be FutureCaptions[1])
+  table.sort(FutureCaptions, function(left, right) return left.timeAppear > right.timeAppear end)
+end
+
+function module.addCurrent(data)
+  -- Get the group
+  local group = data.group
+
+  -- Remove any matching captions of that group (if any)
+  Utilities.removeIf(CaptionList, function(itm) return itm.group == group end)
+
+  -- Get the position of this new caption
+  local pos = CaptionPositions[group]
+
+  -- Is that position available?
+
+  -- (this isn't on that comment)
+  table.insert(CaptionList, 1, data)
 end
